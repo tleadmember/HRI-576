73c95118dfb75fa28f06fa83d3b5ef892a432e27
Bill Huynh <thong.q.huynh@gmail.com>
2024-10-11 14:56:35 -0600

    [feature] lab8

---
+++ lab8/nao_tts.py
@@ -0,0 +1,169 @@
+import time
+import naoqi
+import math
+from naoqi import ALProxy
+
+# tts = ALProxy
+
+ip = "10.60.253.210"  # ADJUST MANUALLY !!!
+
+# text to speech proxy
+tts = ALProxy("ALTextToSpeech", ip, 9559)
+
+# animated speech proxy
+animated_speech = ALProxy("ALAnimatedSpeech", ip, 9559)
+
+# posture proxy
+posture_proxy = ALProxy("ALRobotPosture", ip, 9559)
+
+current_posture = posture_proxy.getPosture()
+
+# gaze-related proxies
+awareness_proxy = ALProxy("ALBasicAwareness", ip, 9559)
+awareness_proxy.setEngagementMode("FullyEngaged")
+awareness_proxy.setEnabled(True)
+# awareness_proxy.setStimulusDetectionEnabled("Touch", True)
+
+# gaze-related proxies
+tracker_proxy = ALProxy("ALTracker", ip, 9559)
+
+text_old = ""
+
+# motion proxy
+motion_proxy = ALProxy("ALMotion", ip, 9559)
+
+
+# Function to convert degrees to radians
+def deg_to_rad(deg):
+    return deg * math.pi / 180
+
+
+right_arm_joints = [
+    "RShoulderPitch",
+    "RShoulderRoll",
+    "RElbowYaw",
+    "RElbowRoll",
+    "RWristYaw",
+    "RHand",
+]
+
+left_arm_joints = [
+    "LShoulderPitch",
+    "LShoulderRoll",
+    "LElbowYaw",
+    "LElbowRoll",
+    "LWristYaw",
+    "LHand",
+]
+
+
+# Flex motion
+def flex_motion():
+    # motion_proxy.setStiffnesses("Shoulder", 1.0)
+    # motion_proxy.setStiffnesses("Elbow", 1.0)
+    # motion_proxy.setStiffnesses("Wrist", 1.0)
+    # motion_proxy.setStiffnesses("Hand", 1.0)
+
+    right_arm_angles_degrees_flex = [-4.6, -68.6, 86.7, 88.5, 75.4, 0.0]
+    right_arm_angles_radians_flex = [
+        deg_to_rad(angle) for angle in right_arm_angles_degrees_flex
+    ]
+    left_arm_angles_degrees_flex = [-8.8, 70.0, -75.1, -87.6, -72.0, 0.02]
+    left_arm_angles_radians_flex = [
+        deg_to_rad(angle) for angle in left_arm_angles_degrees_flex
+    ]
+
+    fractionMaxSpeed = 0.2
+    joints = left_arm_joints + right_arm_joints
+    angles_radians_flex = left_arm_angles_radians_flex + right_arm_angles_radians_flex
+
+    motion_proxy.setAngles(joints, angles_radians_flex, fractionMaxSpeed)
+
+    time.sleep(1.0)
+    # motion_proxy.setStiffnesses("Shoulder", 0.0)
+    # motion_proxy.setStiffnesses("Elbow", 0.0)
+    # motion_proxy.setStiffnesses("Wrist", 0.0)
+    # motion_proxy.setStiffnesses("Hand", 0.0)
+
+
+def zero_motion():
+    # motion_proxy.setStiffnesses("Shoulder", 1.0)
+    # motion_proxy.setStiffnesses("Elbow", 1.0)
+    # motion_proxy.setStiffnesses("Wrist", 1.0)
+    # motion_proxy.setStiffnesses("Hand", 1.0)
+
+    right_arm_angles_degrees_zero = [85.1, -12.7, 67.4, 22.6, 7.1, 0.5]
+    right_arm_angles_radians_zero = [
+        deg_to_rad(angle) for angle in right_arm_angles_degrees_zero
+    ]
+    left_arm_angles_degrees_zero = [82.9, 11.9, -70.1, -23.2, 4.2, 0.5]
+    left_arm_angles_radians_zero = [
+        deg_to_rad(angle) for angle in left_arm_angles_degrees_zero
+    ]
+
+    fractionMaxSpeed = 0.2
+    joints = left_arm_joints + right_arm_joints
+    angles_radians_zero = left_arm_angles_radians_zero + right_arm_angles_radians_zero
+
+    motion_proxy.setAngles(joints, angles_radians_zero, fractionMaxSpeed)
+
+    time.sleep(1.0)
+    # motion_proxy.setStiffnesses("Shoulder", 0.0)
+    # motion_proxy.setStiffnesses("Elbow", 0.0)
+    # motion_proxy.setStiffnesses("Wrist", 0.0)
+    # motion_proxy.setStiffnesses("Hand", 0.0)
+
+
+# Start
+if current_posture != "Stand":
+    # Make NAO stand up
+    posture_proxy.goToPosture("StandInit", 1.0)
+
+# # Call the function for flex motion
+# flex.motion()
+
+# Listening and speaking
+with open("listen.txt", "w") as f:
+    f.write("no")
+
+with open("response.txt", "w") as f:
+    f.write(" ")
+
+while True:
+    try:
+        with open("response.txt", "r") as f:
+            text = f.read().replace("\n", " ")
+
+        # have the NAO speak ChatGPT's response
+        if text != "":
+            if text != text_old:
+                tracker_proxy.lookAt([2, 2, 0], 2, 0.5, False)
+                # animated_speech.say(text)
+
+                trigger_text = "[trigger;flex_motion]"
+
+                # if "[trigger;flex_motion]" in text:
+                #     flex_motion()
+
+                old_index = text.find(trigger_text)
+                new_text = ""
+                if old_index != -1:
+                    index = old_index + len(trigger_text)
+                    new_text = text[:index] + " " + text[index:]
+                    print(new_text)
+                    flex_motion()
+
+                tts.say(new_text)
+
+                if old_index != -1:
+                    zero_motion()
+
+                print(new_text)
+                text_old = new_text
+                with open("listen.txt", "w") as f:
+                    f.write("yes")
+
+        time.sleep(1)
+    except Exception as e:
+        print("An error occurred: ", e)
+        time.sleep(1)
---
+++ lab8/speechOpenAI.py
@@ -0,0 +1,158 @@
+import time
+import multiprocessing
+import speech_recognition as sr
+from openai import OpenAI
+import json
+import os
+
+# import nao_tts
+
+
+# code to figure out the microphone indexes for multi-microphone use
+"""
+microphones = sr.Microphone.list_microphone_names()
+for index, name in enumerate(microphones):
+  print(f"Microphone with index {index} and name \"{name}\" found")
+"""
+
+openAIKey = os.environ["OPENAI_API_KEY"]
+
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# Initialize the OpenAI client
+client = OpenAI(api_key=openAIKey)
+MODEL = "gpt-4"
+
+chat_history = [
+    # {
+    #     "role": "system",
+    #     "content": "You are a NAO robot that provides appropiate gestures while answering my questions breifly. Provide the response in this example format: First part of response ^start(animations/Stand/Gestures/Hey_1) second part of response. ",
+    # },
+    {
+        "role": "system",
+        "content": "You are a coach at children's gym that focuses on brain, body, and character development. You are a leader and a mentor to the kids. You guide them through the activities and lessons. You want them to follow your instruction.",
+    },
+    {"role": "system", "content": "Your name is mentor."},
+    {
+        "role": "system",
+        "content": "Your personality is defined by the big 5 personality traits rated on a scale of 1 to 10.",
+    },
+    {
+        "role": "system",
+        "content": "Your openness trait is 5/10. It is average, you follow instructions but you are not too rigid.",
+    },
+    {
+        "role": "system",
+        "content": "Your concientiousness trait is 9.5/10. You are organized and have a lot of self-control.",
+    },
+    {
+        "role": "system",
+        "content": "Your extraversion trait is 8.5/10. You are social, warm, polite, and can make interactions fun.",
+    },
+    {
+        "role": "system",
+        "content": "Your agreeableness trait is 9/10. You are friendly, understanding, and trustworthy, but you need to set boundaries with the kids to keep their lessons progressing.",
+    },
+    {
+        "role": "system",
+        "content": "Your neuroticism trait is 2/10. It is low, but you are not robotic or machine-like.",
+    },
+    {
+        "role": "system",
+        "content": "Before you say the words 'With me, I am strong, I am brave, I can do it!', include [trigger;flex_motion] in ",
+    },
+]
+
+
+with open("history.txt", "w") as f:
+    json.dump(chat_history, f)
+
+
+def initiate_interaction():
+    # read current chat history
+    with open("history.txt", "r") as f:
+        chat_history = json.load(f)
+
+    # Keeps the chat history with ChatGPT.
+    chat_history.append(
+        {
+            "role": "user",
+            "content": "Hello mentor, you are going to be working with a kid for the next 10 minutes. Your first task is to lead them through an interaction. You should say 'I am strong, I am brave, I can do it.' along with the kid. Do not restate your command, and just do it. Your next response should begin with 'Let's do our Affirmation! Are you ready? With me, I am strong, I am brave, I can do it!' ",
+        }
+    )
+    completion = client.chat.completions.create(model=MODEL, messages=chat_history)
+    response = completion.choices[0].message.content
+    # nao_tts.flex_motion()
+    print("Mentor: " + response)
+
+    # Add the assistant's response to the chat history.
+    chat_history.append({"role": "assistant", "content": response})
+
+    # Save the updated chat history back to the file.
+    with open("history.txt", "w") as f:
+        json.dump(chat_history, f)
+
+    with open("response.txt", "w") as f:
+        f.write(response)
+
+
+def speak(mic, person):
+    with sr.Microphone(device_index=mic) as source:
+        duration = 3
+        print(f"Calibrating ambient noise for {duration} seconds.")
+        r.adjust_for_ambient_noise(source, duration)
+
+        while True:
+
+            while True:
+                with open("listen.txt", "r") as f:
+                    result = f.read()
+                if result == "yes":
+                    break
+
+            try:
+                print("Listening...")
+                audio = r.listen(source, 15, 5)
+                print("Stopped listening.")
+
+                # Using google to transcribe the audio file to text.
+                text = r.recognize_google(audio)
+                print("Mic " + str(mic) + " " + person + " said: " + text)
+
+                # read current chat history
+                with open("history.txt", "r") as f:
+                    chat_history = json.load(f)
+
+                # Keeps the chat history with ChatGPT.
+                chat_history.append({"role": "user", "content": text})
+                completion = client.chat.completions.create(
+                    model=MODEL, messages=chat_history
+                )
+                response = completion.choices[0].message.content
+                print("Mentor: " + response)
+
+                # Add the assistant's response to the chat history.
+                chat_history.append({"role": "assistant", "content": response})
+
+                # Save the updated chat history back to the file.
+                with open("history.txt", "w") as f:
+                    json.dump(chat_history, f)
+
+                with open("listen.txt", "w") as f:
+                    f.write("no")
+
+                with open("response.txt", "w") as f:
+                    f.write(response)
+
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+            time.sleep(3)
+
+
+# Replace the parameters accordingly.
+initiate_interaction()
+# nao_tts.zero_motion()
+time.sleep(3)
+speak(5, "Human")
